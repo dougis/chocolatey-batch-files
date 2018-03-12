46f92a9c68aed124c733aab69e4ee854479ded0d
Doug Hubbard <doug@dougis.com>
2018-03-12 07:55:14 -0700

    Added multi monitor question, and disk space tools

--- ChocoInstallUpdatePackages.bat
+++ ChocoInstallUpdatePackages.bat
@@ -2,6 +2,7 @@
 REM Check to see if we are an admin user
 setlocal EnableDelayedExpansion
 
+
 REM check to see if the script is running as admin (net file returns an error if not)
 NET FILE 1>NUL 2>NUL
 if '%errorlevel%' == '0' ( goto IsAdmin ) else ( goto MustBeAdmin )
@@ -33,8 +34,8 @@ REM Library files etc (runtimes and so on)
 set DevelopmentRuntimes=jre8 jdk8 jdk9 vcredist2005 vcredist2008 vcredist2010 vcredist2012 vcredist2013 vcredist2015 vcredist140 vcredist2017 dotnet4.7.1 directx
 
 REM Items used for software development
-set DevelopmentTools=ctags visualstudiocode sublimetext3 notepadplusplus sqlite winmerge git gitextensions gitkraken sqlitebrowser vagrant virtualbox vagrant-manager docker-toolbox hosts.editor postman
-set Work_DevelopmentTools=crystalreports2010runtime tortoisesvn autoit scite4autoit3 androidstudio meld
+set DevelopmentTools=ctags visualstudiocode sublimetext3 notepadplusplus sqlite winmerge git gitextensions gitkraken sqlitebrowser vagrant virtualbox vagrant-manager docker-toolbox hosts.editor postman meld
+set Work_DevelopmentTools=crystalreports2010runtime tortoisesvn autoit scite4autoit3 androidstudio
 
 REM Font packages to install
 set Fonts=inconsolata sourcecodepro droidsansmono oxygenmono
@@ -43,9 +44,9 @@ REM Web browsers
 set Browsers=googlechrome firefox opera
 
 REM General tools used on my systems
-set SystemUtilities=procmon procexp f.lux ditto rainmeter sysmon 7zip winrar vlc paint.net gpg4win classic-shell ccleaner sysinternals curl wget greenshot freefilesync wizmouse nitroreader.install
+set SystemUtilities=procmon procexp f.lux ditto rainmeter sysmon 7zip winrar vlc paint.net gpg4win classic-shell ccleaner sysinternals curl wget greenshot freefilesync wizmouse nitroreader.install windirstat treesizefree
 
-set WorkSystemUtilities=bginfo slack toggl displayfusion virtuawin
+set WorkSystemUtilities=bginfo slack toggl virtuawin
 
 REM SSH/File Transfer and Passwords
 set SSH_Password_TransferTools=putty superputty openssh filezilla lastpass
@@ -60,6 +61,16 @@ set VMWareTools=vmware-horizon-client vmwarevsphereclient
 set Games=steam origin goggalaxy uplay
 set SharingTools=dropbox
 
+:ChooseMonitors
+set /P DualMonitors=Do you have more than 1 monitor[Y/N]? (X to exit)
+if /I "%DualMonitors%" EQU "Y" goto :HaveMultipleMonitors
+if /I "%DualMonitors%" EQU "N" goto :PackageChoice
+if /I "%DualMonitors%" EQU "X" goto :End
+
+goto :ChooseMonitors
+
+:HaveMultipleMonitors
+set SystemUtilities=%SystemUtilities% displayfusion
 
 :PackageChoice
 set /P Machine=Is this for Home or Work[H/W]? (X to exit)
@@ -80,6 +91,7 @@ REM for work
 set Items=ChocolateyTools DevelopmentRuntimes DevelopmentTools Work_DevelopmentTools Fonts Browsers SystemUtilities WorkSystemUtilities SSH_Password_TransferTools Work_SSH_Password_TransferTools RemoteTools VMWareTools
 
 :RunIt
+
 (for %%a in (%Items%) do (
    echo %CHOCO_LABEL% items from %%a
    (for %%b in (!%%a!) do (
